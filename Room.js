2c4f73248bfe9392c517bb45bcf68941f9d8aa84
Yehudit <Y0527187734@gmail.com>
2026-04-15 13:05:50 +0300

    add Room schema

---
+++ Room.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    wing: { type: String, required: true },
+    floor: { type: Number, required: true },
+    capacity: { type: Number, required: true },
+    hasProjector: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Room', roomSchema);
