d94b79bd3179a548da04ed37067281b4a777edab
Rachel gratsh <r27175847@gmail.com>
2026-04-28 16:46:42 +0300

    feat: define cancellation schema and link to room

--- server/models/Room.js
+++ server/models/Room.js
@@ -4,7 +4,12 @@ const roomSchema = new mongoose.Schema({
   wing: { type: String, required: true },
   floor: { type: Number, required: true },
   size: { type: Number, required: true },
-  hasProjector: { type: Boolean, default: false }
+  hasProjector: { type: Boolean, default: false }, 
+  
+  cancellations: [{ 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'Cancellation' 
+  }]
 });
 
 module.exports = mongoose.model('Room', roomSchema);
